8e8d5956e718fa44ea4e52d0e56bca96735b78b2
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-10-09 20:58:35 -0700

    Completed initialize and valid_move?

--- W2D2-homework/lib/board.rb
+++ W2D2-homework/lib/board.rb
@@ -2,13 +2,22 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) { Array.new }
+    place_stones
+    @name1 = name1
+    @name2 = name2
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, idx|
+      4.times { cup << :stone unless idx == 6 || idx == 13 }
+    end
   end
 
   def valid_move?(start_pos)
+    #start pos is index -- adjust for user input
+    raise 'Invalid starting cup' unless (1..12).to_a.include?(start_pos)
   end
 
   def make_move(start_pos, current_player_name)
