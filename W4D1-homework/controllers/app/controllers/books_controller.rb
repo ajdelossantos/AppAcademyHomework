2b81b6d46e3140549776ff6a08dd817e6a963a9b
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-10-22 23:14:01 -0700

    Add #new and #create methods to BooksController

--- W4D1-homework/controllers/app/controllers/books_controller.rb
+++ W4D1-homework/controllers/app/controllers/books_controller.rb
@@ -1,27 +1,32 @@
 class BooksController < ApplicationController
   def index
-    # your code here
-
     @books = Book.all
     render :index
   end
 
   def new
-    # your code here
+    render :new
   end
 
   def create
-    # your code here
+    book = Book.new(book_params) # Creates new Book object with params passed-in
+    #TODO Ask a TA question -- what is going on here? (Adapted from solutions)
+    if book.save
+      redirect_to books_url
+    else
+      flash.now[:errors] = book.errors.full_messages
+      render :new
+    end
   end
 
   def destroy
-    # your code here
     book = Book.find_by(params[:id])
     book.destroy
     redirect_to books_url
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title, :author)
   end
