066781358bdb378689622f7f13025adb50e123f8
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-10-24 08:52:41 -0700

    Add _form and _book partials

--- W4D2-demo/controllers/app/controllers/books_controller.rb
+++ W4D2-demo/controllers/app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
 
   #TODO Use redirect in show method to go back to index b/c encapsulation
   # One redirect per method
+
   def show
     @book = Book.find_by(id: params[:id])
     # render json: @book
@@ -19,22 +20,45 @@ class BooksController < ApplicationController
     end
   end
 
+  def new
+    # NO params
+    @book = Book.new
+    render :new
+  end
 
+  def create
+    # create, then save!
+    @book = Book.new(book_params)
+    # params = { book: {title: 'The Subtle...'}}
 
-  # def new
-  #   render :new
-  # end
-  #
-  # def create
-  #   book = Book.new(book_params) # Creates new Book object with params passed-in
-  #   #TODO Ask a TA question -- what is going on here? (Adapted from solutions)
-  #   if book.save
-  #     redirect_to books_url
-  #   else
-  #     flash.now[:errors] = book.errors.full_messages
-  #     render :new
-  #   end
-  # end
+    if @book.save
+      # show user
+      # Rails will automatically picku ID from @books
+      redirect_to book_url(@book)
+    else
+      flash.now[:errors] = book.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+
+    if @book.update_attributes(book_params)
+      # show user
+      # Rails will automatically pickup ID from @books
+      redirect_to book_url(@book)
+    else
+      flash.now[:errors] = book.errors.full_messages
+      render :edit
+    end
+  end
+
   #
   # def destroy
   #   book = Book.find_by(params[:id])
@@ -42,9 +66,9 @@ class BooksController < ApplicationController
   #   redirect_to books_url
   # end
 
-  # private
+  private
 
-  # def book_params
-  #   params.require(:book).permit(:title, :author)
-  # end
+  def book_params
+    params.require(:book).permit(:title, :author, :year, :category, :description)
+  end
 end
