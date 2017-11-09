fcfbc00e8177c397524f88559e559a119b27762d
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-11-09 08:24:58 -0800

    Add PhaseII

--- W6D4-homework/script.js
+++ W6D4-homework/script.js
@@ -19,13 +19,31 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding SF places as list items
 
   // --- your code here!
+  const addFavorites = (event) => {
+    event.preventDefault();
+
+    // IDEA looked over solutions--why document.querySelector?
+    const favoriteInput = document.querySelector(".favorite-input");
+    const favorite = favoriteInput.value;
+    favoriteInput.value = "";
+
+    const li = document.createElement("li");
+    li.textContent = favorite;
+
+    const sfPlaces = document.getElementById("sf-places");
+    sfPlaces.appendChild(li);
+  };
+
+  // IDEA looked over solutions--how do you handle submissions?
+  const submitButton = document.querySelector(".favorite-submit");
+  submitButton.addEventListener("click", addFavorites);
 
 
 
   // adding new photos
 
   // --- your code here!
-
+  //TODO Very lost here, timed out. Ask a TA later!
 
 
 });
