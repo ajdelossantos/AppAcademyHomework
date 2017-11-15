3a844e14dbdf0f6e6f1e7280407fb58e96befd56
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-11-15 07:14:53 -0800

    Finish Phase 1

--- W7D3-homework/giphy_search.jsx
+++ W7D3-homework/giphy_search.jsx
@@ -2,3 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+
+//TODO remove after debugging
+// import { fetchSearchGiphys } from './util/api_util';
+import { receiveSearchGiphys, fetchSearchGiphys }from './actions/giphy_actions';
+
+const debugVars = (store) => {
+  window.store = store;
+  window.fetchSearchGiphys = fetchSearchGiphys;
+  window.receiveSearchGiphys = receiveSearchGiphys;
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  //TODO remove after debugging
+  debugVars(store);
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={ store } />, root);
+});
